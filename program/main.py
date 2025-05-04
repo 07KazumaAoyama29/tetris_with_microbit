553b006966a7cc085424ef72e601e3ff5eb0b72e
reckless0729 <reckless200207292020@gmail.com>
2025-05-04 23:29:19 +0900

    update

--- program/main.py
+++ program/main.py
@@ -2,37 +2,40 @@
 from microbit import *
 import random
 def flash(x, y, light):
-    display.set_pixel(x, y, light)
+  display.set_pixel(x, y, light)
 def clear():
-    display.clear()
+  display.clear()
 def flashm(list):
-   for i in range(5):
-      for j in range(5):
-         if list[j][i] != 0 : flash(i, j, 9)
+  for i in range(5):
+    for j in range(5):
+      flash(i, j, list[j][i] * 9)
 
 blocks = [
-  [0, 0, 0, 0, 1],  # y=0
+  [0, 0, 0, 0, 0],  # y=0
   [0, 0, 0, 0, 0],  # y=1
-  [0, 0, 1, 0, 0],  # y=2
+  [0, 0, 0, 0, 0],  # y=2
   [0, 0, 0, 0, 0],  # y=3
-  [0, 0, 0, 0, 1],  # y=4
+  [0, 0, 0, 0, 0],  # y=4
+  [1, 1, 1, 1, 1],  # 地面
 ]
-
 # === ここから書いてOK ===
 x = 2
 y = 0
 timer = 0
 while True:
+  music.play(music.POWER_UP)
+  flashm(blocks)
   flash(x, y, 9)
   timer += 1
-  if timer % 10000 == 0:
+  if timer % 1000 == 0:
     clear()
     y += 1
-    if y > 4 : y = 0
+    if blocks[y][x] != 0:
+       blocks[y-1][x] = 1
+       y = 0   
   if button_a.was_pressed() and x > 0:
         x -= 1
         clear()
   if button_b.was_pressed() and x < 4:
         x += 1
         clear()
-flashm(blocks)
